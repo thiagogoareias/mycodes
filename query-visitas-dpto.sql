14946a26d1c0429bef72286bf906729496d22ce8
thiagogoareias <96317501+thiagogoareias@users.noreply.github.com>
2021-12-17 18:58:58 -0300

    Update query-visitas-dpto.sql

--- query-visitas-dpto.sql
+++ query-visitas-dpto.sql
@@ -5,12 +5,11 @@ SELECT
       depto,
       device,
       SUM(visitas) as visitas,
-      origem,
 FROM `seo-analytics-219818.evaluated_navigation.visitas_hits` 
 WHERE partition_date between "2021-09-01" AND "2021-11-30"
   AND brand = "ACOM"
       AND depto IN ("INFORMÁTICA E ACESSÓRIOS (60)", "GERAL")
       #AND depto = "GERAL"
       AND origem = "adobe"
-GROUP BY 1,2,3,4,5,7
+GROUP BY 1,2,3,4,5
 ORDER BY 2,1,3,5 ASC
